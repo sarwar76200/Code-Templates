303593d5828ceba70dd52cf6202386f184ad896a
Sarwar Alam <sarwar76200@gmail.com>
2023-02-11 00:13:49 +0600

    Update run.bat

--- VSCode Setup/run.bat
+++ VSCode Setup/run.bat
@@ -2,28 +2,34 @@
 set file_name=%1
 set file_name_without_ext=%2
 
+set exit_code=0
+set config=null
 
 for /f %%i in (build_configuration) do (
     if %%i == release (
-        call :release
-    ) else (
-        call :debug
-    )
-    call :run_file
-    exit /b
+        set %config=release
+    ) 
+    if %%i == debug (
+        set %config=debug
+    ) 
 )
 
-exit /b
+call :compile
+call :run
+exit /b %exit_code%
 
-:debug
-g++ -std=c++20 -DLOCAL %file_name% -o %file_name_without_ext%
+:compile
+if %config% == release (
+    echo [release] g++ -std=c++20 -DLOCAL -O2 %file_name% -o %file_name_without_ext%
+    g++ -std=c++20 -DLOCAL -O2 %file_name% -o %file_name_without_ext%
+)
+if %config% == debug (
+    echo [debug] g++ -std=c++20 -DLOCAL -O2 %file_name% -o %file_name_without_ext%
+    g++ -std=c++20 -DLOCAL %file_name% -o %file_name_without_ext%
+)
 exit /b 0
 
-:release
-g++ -std=c++20 -DLOCAL -O2 %file_name% -o %file_name_without_ext%
-exit /b 0
-
-
-:run_file
+:run
 %file_name_without_ext% < in.txt > out.txt 2>err.txt
+set /a exit_code=%ErrorLevel%+0
 exit /b 0
